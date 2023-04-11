076ac78e58e1b113dce48210e250cfda79d97f4e
GlitchLagz <130423151+GlitchLagz@users.noreply.github.com>
2023-04-11 12:15:04 +0930

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpawson <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/23 09:46:08 by tpawson           #+#    #+#             */
+/*   Updated: 2023/04/07 13:27:11 by tpawson          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# ifndef OPEN_LIMIT
+#  define OPEN_LIMIT 254
+# endif
+
+char	*get_next_line(int fd);
+
+/* UTIL FUNCTS */
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
